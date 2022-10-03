a29a34a4456529f9b51a40741d28245808815b8d
Fadey <notchenkofaddey@gmail.com>
2022-10-03 18:54:22 +0300

    Initial Commit

---
+++ Memories/MemoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriesApp.swift
+//  Memories
+//
+//  Created by Fadey Notchenko on 03.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MemoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
